5a9413f0d186239db105c70fa6036d8db300a65b
bgseonsu <90617191+bgseonsu@users.noreply.github.com>
2021-09-14 01:07:28 +0900

    Add files via upload

--- img/img.js
+++ img/img.js
@@ -1,1 +1,13 @@
-.
+const imgFile = {
+    "head" : [1, 2, 3, 4, 5, 6, 7, 8],
+    "pierce" : ["Cross", "Gold1", "Gold2", "Gold3", "Gold3", "Metal1", "Metal2", "Metal3", "Metal3"],
+    "clothes" : [1, 2, 3, 4],
+    "shoes" : [1, 2, 3],
+    "acc" : [1, 2],
+    "hand" : [1, 2, 3, 4, 5],
+    "char" : [1, 2, 3, 4, 5]
+}
+
+const bgColor = ['crimson', 'tomato', 'teal', 'slateblue', 'dimgray'];
+
+const characterFile = ["char1","char2","char3"];
